4e814765b0c0d4a5cbd7c30044400adc51e4470f
stdio666 <115464395+stdio666@users.noreply.github.com>
2022-10-13 21:22:43 +0300

    Update IBank_part2.py

--- Module8/practice/IBank/IBank_part2.py
+++ Module8/practice/IBank/IBank_part2.py
@@ -1,45 +1,84 @@
-from typing import List
-# Сюда отправляем готовое решение IBank часть-2
 
-# класс для хранение информации об операциях
-class Operation:
-    # Типы операций храним в свойствах класса
-    DEPOSIT = 'Пополнение'
-    WITHDRAW = 'Снятие'
-    TRANSFER_OUT = 'Перевод'
-    TRANSFER_IN = 'Поступление'
+import re
+class Account:
+    def __init__(self, name: str, passport: str, phone_number: str, start_balance: int = 0):
+        self.name = name
+        self.passport = passport
+        self.phone_number = phone_number
+        # self.balance = start_balance
+        self.__balance = start_balance  # TODO: Закрываем прямой доступ к балансу
+        self.hystory =[]
 
-    # Напоминаю: обращение к этим переменным происходит через имя класса, пример: Operation.WITHDRAW
-
-    def __init__(self, ):
-        # TODO: Тут добавляем свойства для хранение информации об операции
-        pass
+    def full_info(self) -> str:
+        """
+        Полная информация о счете в формате: "Иван баланс: 100 руб. паспорт: 3200 123456 т.+7-900-200-02-03"
+        """
+        return f"{self.name} баланс: {self.balance} руб. паcпорт: {self.passport} т.{self.phone_number}"
 
     def __repr__(self) -> str:
         """
-        :return: возвращает строковое представление операции. Формат указан в 02_IBank_part2.md
+        :return: Информацию о счете в виде строки в формате "Иван баланс: 100 руб."
         """
-        pass
+        return f"{self.name} баланс: {self.balance} руб."
 
+    @property
+    def balance(self) -> int:
+        return self.__balance
 
-class Account:
-    # TODO: сюда копируем реализацию класса Account из предыдущей задачи
-    # TODO: историю храним как список объектов класса Operation, добавив свойство в конструктор:
-    #   self.__history = []
-
-    # Данный метод дан в готовом виде. Изучите его и используйте как пример, для доработки других
-    def deposit(self, amount: int, to_history: bool = True) -> None:
+    def deposit(self, amount: int) -> None:
         """
         Внесение суммы на текущий счет
         :param amount: сумма
-        :param to_history: True - записывать операцию в историю, False - не записывать
         """
+        self.hystory.append(f"Пополнено {amount}")
         self.__balance += amount
-        if to_history:
-            self.__history.append(Operation(amount, Operation.DEPOSIT))
 
-    def get_history(self) -> List[str]:
+    def withdraw(self, amount: int) -> None:
         """
-        :return: возвращаем историю операций  виде списка строковых представлений
+        Снятие суммы с текущего счета
+        :param amount: сумма
         """
-        return [str(operation) for operation in self.__history]
+        if self.balance < (amount + amount * tax/100):
+            self.hystory.append("Недостаточно для перевода")
+        else:
+            self.__balance -= amount
+            self.__balance = self.balance - amount * tax/100
+            self.hystory.append(f"Снято {amount} (Комиссия {tax} %)")
+
+    def trans(self, dest, amount: int):
+        if self.balance >= amount + amount * tax/100:
+            dest.deposit(amount)
+            self.withdraw(amount)
+            self.hystory.append(f"Перевод -> {dest.name}  {amount} (Комиссия {tax} %)")
+            dest.hystory.append(f"Получено <- от {self.name} {amount}")
+        else:
+            self.hystory.append("Недостаточно для перевода")
+
+regex_pas = "^[0-9\s]*$"
+regex_tel = "^\+?[78][-\(]?\d{3}\)?-?\d{3}-?\d{2}-?\d{2}$"
+
+name1 = "Иван";    pas1 = "3230 634563"; tel1 = "+7-900-765-12-34"; bal1 = 1000
+name2 = "Алексей"; pas2 = "3232 456124"; tel2 = "+7-901-744-22-99"; bal2 = 200
+tax   = 6
+
+if re.match(regex_tel, tel1) and re.match(regex_pas, pas1):
+    account1 = Account(name1, pas1, tel1, bal1)
+    print(account1.full_info())
+else:
+    print("Телефон или паспорт указан неверно")
+if re.match(regex_tel, tel2) and re.match(regex_pas, pas2):
+    account2 = Account(name2, pas2, tel2, bal2)
+    print(account2.full_info())
+else:
+    print("Телефон или паспорт указан неверно")
+
+account2.withdraw(100)
+print(account2.full_info())
+account2.deposit(1100)
+print(account2.full_info())
+account2.trans(account1, 20)
+account1.trans(account2, 500)
+print(account1, account1.hystory)
+print(account2, account2.hystory)
+
+
